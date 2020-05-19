81568be1d19949754077fa9e409c54e6c06590c8
Sarath Shanker <shankersarath@gmail.com>
2020-05-19 10:38:24 -0500

    first

---
+++ src/build/precache-manifest.6b9f37b20fb077525037fad8acb3f8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dfb05a808018a2e318ec0d276edea3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d653d2ea28f573b694",
+    "url": "/static/css/main.dbdb0f3f.chunk.css"
+  },
+  {
+    "revision": "7d365ec552a477020685",
+    "url": "/static/js/2.93d51652.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93d51652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d653d2ea28f573b694",
+    "url": "/static/js/main.c727a3fa.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
